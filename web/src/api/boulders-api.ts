ef04d6040937e08e0d59da149b5fd2fa4da4cccc
Caleb Burke <calebburke91@gmail.com>
2026-03-11 17:21:21 -0700

    easy fix

---
+++ web/src/api/boulders-api.ts
@@ -0,0 +1,55 @@
+import http from "@/api/http-client"
+
+export type Boulder = {
+  id: number
+  name: string
+  description: string
+  grade: string
+  location: string
+  createdAt: string
+  updatedAt: string
+}
+
+export type BoulderWhereOptions = {
+  grade?: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export type BoulderFiltersOptions = {}
+
+export type BoulderQueryOptions = {
+  where?: BoulderWhereOptions
+  filters?: BoulderFiltersOptions
+  page?: number
+  perPage?: number
+}
+
+export const bouldersApi = {
+  async list(params: BoulderQueryOptions = {}): Promise<{
+    boulders: Boulder[]
+    totalCount: number
+  }> {
+    const { data } = await http.get("/api/boulders", {
+      params,
+    })
+    return data
+  },
+  async get(boulderId: number): Promise<{ boulder: Boulder }> {
+    const { data } = await http.get(`/api/boulders/${boulderId}`)
+    return data
+  },
+  async create(attributes: Partial<Boulder>): Promise<{ boulder: Boulder }> {
+    const { data } = await http.post("/api/boulders", attributes)
+    return data
+  },
+  // async update(boulderId: number, attributes: Partial<Boulder>): Promise<{ boulder: Boulder }> {
+  //   const { data } = await http.patch(`/api/boulders/${boulderId}`, attributes)
+  //   return data
+  // },
+  // async delete(boulderId: number): Promise<void> {
+  //   const { data } = await http.delete(`/api/boulders/${boulderId}`)
+  //   return data
+  // },
+}
+
+export default bouldersApi
