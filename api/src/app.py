8516833b6bb01c9b442ad832e818f766c7f8da6c
Amitoj Singh <amitojsingh366@gmail.com>
2026-03-16 14:38:27 -0700

    add env var for FRONTEND_URLS

--- api/src/app.py
+++ api/src/app.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, origins=["http://localhost:8080"])
+    CORS(app, origins=Config.FRONTEND_URLS)
 
     db.init_app(app)
 
--- api/src/config.py
+++ api/src/config.py
@@ -13,6 +13,7 @@ class Config:
     TZ = os.getenv("TZ", "UTC")
     API_PORT = os.getenv("API_PORT", "5000")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    FRONTEND_URLS = os.getenv("FRONTEND_URLS", "http://localhost:8080").split(",")
     
     if not SQLALCHEMY_DATABASE_URI:
         raise ValueError("SQLALCHEMY_DATABASE_URI environment variable is not set")
